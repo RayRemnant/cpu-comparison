ab89531b3b48f19737acbd9d079176547acb013f
Damian Florin Dragos <ddragos-external@soldo.com>
2025-05-02 00:35:32 +0200

    edit css

--- src/components/ui/CompareButton.tsx
+++ src/components/ui/CompareButton.tsx
@@ -11,7 +11,7 @@ interface CompareButtonProps {
 export const CompareButton: React.FC<CompareButtonProps> = ({ cpu, onCompare, isSelected }) => {
   return (
     <div
-      className="cursor-pointer rounded-full p-2 bg-[radial-gradient(circle,rgba(255,255,255,0.5),rgba(255,255,255,0))]"
+      className="cursor-pointer p-2 flex items-center"
       onClick={() => onCompare(cpu)}
     >
       {isSelected ? <Minus className="h-4 w-4" /> : <Plus className="h-4 w-4" />}
